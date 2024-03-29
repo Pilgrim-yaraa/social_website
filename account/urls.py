ebf5a1133862c9c17c50172e2ea0dae4be4c516d
Yaroslav <yaroslav_sim97@mail.ru>
2024-03-29 16:44:43 +1100

    добавил форму регистрации пользователя

--- account/urls.py
+++ account/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 from django.contrib.auth import views as auth_views
 
@@ -19,6 +19,7 @@ urlpatterns = [
     # path('password-reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     # path('password-reset/complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 
-    path('', include(django.contrib.auth.urls)),
+    path('', include('django.contrib.auth.urls')),
     path('', views.dashboard, name='dashboard'),
+    path('register/', views.register, name='register')
 ]
